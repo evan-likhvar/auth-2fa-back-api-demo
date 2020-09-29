4a3c487b5863229b92ce2822498d6a07e14c3d55
Sergios22530 <SergeyDovzhanutsia@gmail.com>
2020-09-29 17:48:12 +0300

    'working_with_settings_module'

--- app/Modules/v1/Settings/Http/Requests/SettingStoreRequest.php
+++ app/Modules/v1/Settings/Http/Requests/SettingStoreRequest.php
@@ -4,7 +4,7 @@ namespace App\Modules\v1\Settings\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class SettingsStoreRequest extends FormRequest
+class SettingStoreRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
